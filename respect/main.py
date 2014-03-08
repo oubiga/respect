4c9fed5e2cb9a3f8583f9776b47ef38340200515
Pablo Oubiña <oubiga@yahoo.es>
2014-03-08 13:58:36 +0100

    Added main.py module, entry point for the respect command

---
+++ respect/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Respect.
+
+Usage:
+    main.py <username> <email>
+
+Options:
+    -h, --help    Show this screen and exit.
+
+"""
+
+from docopt import docopt 
+
+
+def main():
+    """
+    Main entry point for the `respect` command.
+
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
