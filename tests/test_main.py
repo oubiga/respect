4b3e238ac36e12cdcafdedc8429645554520f42b
Pablo Oubiña <oubiga@yahoo.es>
2014-03-10 11:58:41 +0100

    Added test_github_basic_authenthication and test_github_get_not_authenthicated_user to test_main.py module

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,6 +9,8 @@ Tests for `respect.main` module.
 """
 
 import sys
+import requests
+from urlparse import urljoin
 
 from respect import main
 
@@ -16,6 +18,9 @@ if sys.version_info[:2] < (2, 7):
     import unittest2 as unittest
 else:
     import unittest
+
+GITHUB_BASE_URL = 'https://api.github.com'
+GITHUB_USER = 'user'
 
 
 class TestArgParsing(unittest.TestCase):
@@ -25,6 +30,20 @@ class TestArgParsing(unittest.TestCase):
         self.assertEqual(args['<username>'], 'oubiga')
         self.assertEqual(args['<email>'], 'oubiga@yahoo.es')
         self.assertIsInstance(args, dict)
+
+
+class TestGithubBasicAuthentication(unittest.TestCase):
+
+    def test_github_basic_authenthication(self):
+        r = requests.get(GITHUB_BASE_URL)
+        self.assertEqual(r.status_code, 200)
+
+
+class TestGithubGetAuthenticatedUser(unittest.TestCase):
+
+    def test_github_get_not_authenthicated_user(self):
+        r = requests.get(urljoin(GITHUB_BASE_URL, GITHUB_USER))
+        self.assertEqual(r.status_code, 401)
 
 
 
