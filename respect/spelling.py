1c3e428f778ceeb43874643c72439e194a70def4
Pablo Oubiña <oubiga@yahoo.es>
2014-03-15 00:57:42 +0100

    Added for loop to starred_users_by_language generator

--- respect/spelling.py
+++ respect/spelling.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+import sys 
+
 import requests
+
+if sys.version < '3':
+    from urlparse import urljoin
+else:
+    from urllib.parse import urljoin
 
 
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
@@ -18,5 +26,22 @@ def starred_users_by_languages():
     Used in misspelling input before triger warning prompt to the user. 
 
     """
+    for language in LANGUAGES:
+        #r = requests.get('https://api.github.com/search/users?q=language:%s&sort=stars&per_page=100' % (language,))
+
+        params = {'q': "language:{0}".format(language), 'sort': 'stars', 'per_page': 100}
+        try:
+            r = requests.get(urljoin(BASE_URL, SEARCH_USERS_URL), params=params)
+            yield r.json()['items']
+        except Exception:
+            # because of py3, getting the exception object through sys.exc_info()
+            e = sys.exc_info(1)
+            pass
+
+        
+        
+
+
+def spellchecker():
     pass
 
