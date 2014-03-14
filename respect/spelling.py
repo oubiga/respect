590b4a5bbef8ded1b40fdc47cff8bc726be39e05
Pablo Oubiña <oubiga@yahoo.es>
2014-03-14 10:51:55 +0100

    Added constants ALPHABET, LANGUAGES, URLS and function starred_users_by_languages.

--- respect/spelling.py
+++ respect/spelling.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import requests
+
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+LANGUAGES = (
+    'javascript', 'ruby', 'python', 'java', 'php', 'c', 'c++', 'objective-c', 'shell', 'c#'
+    )
+BASE_URL = 'https://api.github.com/'
+SEARCH_USERS_URL = '/search/users'
+
+
+def starred_users_by_languages():
+    """
+    Generator which returns the most starred users by each language.
+    Used in misspelling input before triger warning prompt to the user. 
+
+    """
+    pass
+
