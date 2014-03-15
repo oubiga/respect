426a04901eae00edb366ab77eff9381d426263ff
Pablo Oubiña <oubiga@yahoo.es>
2014-03-15 13:07:42 +0100

    Deleted DOC variable in parse_respect_args, now getting docstring through f.__doc__.

--- respect/main.py
+++ respect/main.py
@@ -5,23 +5,34 @@ import sys
 from getpass import getpass
 
 from docopt import docopt 
+import requests
 
+from .spelling import spellchecker
+
+if sys.version < '3':
+    from urlparse import urljoin
+else:
+    from urllib.parse import urljoin
+
+GITHUB_BASE_URL = 'https://api.github.com'
+GITHUB_USERS = urljoin(GITHUB_BASE_URL, 'users')
+GITHUB_USER = 'user'
 
 def parse_respect_args(args):
-    DOC = '''
-            Respect
+    '''
+    Respect
 
-            Usage:
-                respect get <username> [--vcs=<name>]
-                respect <username> <email>
-                respect <username> <email> [--speed=<kn>]
+    Usage:
+        respect <username> [--vcs=<name>]
+        respect <username> <email>
+        respect <username> <email> [--speed=<kn>]
 
-            Options:
-                -h, --help    Show this information.
-                --vcs=<name>  Version control system to explore [default: 'gh'].
+    Options:
+        -h, --help    Show this information.
+        --vcs=<name>  Version control system to explore [default: 'gh'].
 
-            '''
-    args = docopt(DOC, argv=args)
+    '''
+    args = docopt(parse_respect_args.__doc__, argv=args)
     return args
 
 
@@ -32,7 +43,15 @@ def main():
     """
 
     args = parse_respect_args(sys.argv[1:])
-    return args
+    r = requests.get(urljoin(GITHUB_USERS, args['<username>']))
+    print args
+
+    if r.status_code == 404 or r.status_code == 403:
+        print 'input the username and password'
+    elif r.status_code == 200:
+        print "yes"
+    else:
+        print 'no'
 
 
 if __name__ == '__main__':
