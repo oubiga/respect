2c94174cf95f70c579835b01e341ddd1e3523746
Pablo Oubiña <oubiga@yahoo.es>
2014-03-17 00:49:43 +0100

    Getting started with endpoints in the dispatch.py module.

--- respect/dispatch.py
+++ respect/dispatch.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+ENDPOINTS = {
+    'username': 'https://api.github.com/users/',
+    'stars': 'https://api.github.com/search/repositories?q=user:pydanny&page=1',
+}
