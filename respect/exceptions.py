ac7c16c4bdb4beb51ef6021592b8d2843a6c63a2
Pablo Oubiña <oubiga@yahoo.es>
2014-03-24 19:14:34 +0100

    Added RespectException, AllowedLanguagesException, UnknownStausCodeException and ConnectionErrorException classes to exceptions.py

--- respect/exceptions.py
+++ respect/exceptions.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+respect.exceptions
+------------------
+
+All exceptions used in the Respect code base are defined here.
+"""
+
+class RespectException(Exception):
+    """
+    Base exception class. All Cookiecutter-specific exceptions should subclass
+    this class.
+    """
+
+class AllowedLanguagesException(RespectException):
+    """
+    Raised when the language input is not defined by Github.
+
+    """
+
+class UnknownStausCodeException(RespectException):
+    """
+    Raseid when the status code is not 404, 403 or 200.
+
+    """
+
+class ConnectionErrorException(RespectException):
+    """
+    Raseid when "requests" is not able to establish the connection.
+
+    """
