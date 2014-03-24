cf055d73c108a76038246faf18663783855bc4a9
Pablo Oubiña <oubiga@yahoo.es>
2014-03-24 19:11:31 +0100

    Fixed sanitize_qualifiers function to deal with c++ language, keeping it unchanged.

--- respect/utils.py
+++ respect/utils.py
@@ -20,34 +20,31 @@ if PY3:
 else:
     input = raw_input
 
-
 if sys.version < '3':
     from urlparse import urljoin
 else:
     from urllib.parse import urljoin
 
-GITHUB_BASE_URL = 'https://api.github.com'
 GITHUB_USERS = 'https://api.github.com/users/'
-GITHUB_USER = 'user'
 
-ALLOWED_LANGUAGES = ['', 'abap', 'actionscript', 'ada', 'agda', 'antlr', 'apex', 'applescript', 'arc',
-'arduino', 'asp', 'assembly', 'augeas', 'autohotkey', 'autoit', 'awk', 'blitzbasic', 'bluespec',
-'boo', 'brightscript', 'bro', 'c', 'c#', 'c++', 'ceylon', 'clean', 'clips', 'clojure', 'cobol',
-'coffeescript', 'coldfusion', 'coq', 'crystal', 'css', 'd', 'dart', 'dm', 'dot', 'dylan', 'ec',
-'eiffel', 'elixir', 'elm', 'erlang', 'f#', 'factor', 'fancy', 'fantom', 'forth', 'fortran', 'glyph',
-'go', 'gosu', 'groovy', 'haskell', 'haxe', 'idl', 'idris', 'io', 'ioke', 'j', 'java', 'javascript',
-'julia', 'kotlin', 'krl', 'lasso', 'livescript', 'logos', 'logtalk', 'lua', 'm', 'markdown',
-'matlab', 'max', 'mirah', 'monkey', 'moonscript', 'nemerle', 'nesc', 'netlogo', 'nimrod', 'nu',
-'objective-c', 'objective-j', 'ocaml', 'omgrofl', 'ooc', 'opa', 'oxygene', 'parrot', 'pascal',
-'perl', 'php', 'pike', 'pogoscript', 'powershell', 'processing', 'prolog', 'puppet', 'python', 'r',
-'racket', 'rdoc', 'realbasic', 'rebol', 'robotframework', 'rouge', 'ruby', 'rust', 'scala',
-'scheme', 'scilab', 'self', 'shell', 'slash', 'smalltalk', 'squirrel', 'supercollider', 'tcl',
-'tex', 'turing', 'txl', 'typescript', 'unrealscript', 'vala', 'verilog', 'vhdl', 'viml', 'volt',
-'wisp', 'xbase', 'xc', 'xml', 'xproc', 'xquery', 'xslt', 'xtend']
+ALLOWED_LANGUAGES = ['', 'abap', 'actionscript', 'ada', 'agda', 'antlr', 'apex', 'applescript', 
+'arc', 'arduino', 'asp', 'assembly', 'augeas', 'autohotkey', 'autoit', 'awk', 'blitzbasic', 
+'bluespec', 'boo', 'brightscript', 'bro', 'c', 'c#', 'c++', 'ceylon', 'clean', 'clips', 'clojure',
+'cobol', 'coffeescript', 'coldfusion', 'coq', 'crystal', 'css', 'd', 'dart', 'dm', 'dot', 'dylan',
+'ec', 'eiffel', 'elixir', 'elm', 'erlang', 'f#', 'factor', 'fancy', 'fantom', 'forth', 'fortran',
+'glyph', 'go', 'gosu', 'groovy', 'haskell', 'haxe', 'idl', 'idris', 'io', 'ioke', 'j', 'java',
+'javascript', 'julia', 'kotlin', 'krl', 'lasso', 'livescript', 'logos', 'logtalk', 'lua', 'm',
+'markdown', 'matlab', 'max', 'mirah', 'monkey', 'moonscript', 'nemerle', 'nesc', 'netlogo',
+'nimrod', 'nu', 'objective-c', 'objective-j', 'ocaml', 'omgrofl', 'ooc', 'opa', 'oxygene', 'parrot',
+'pascal', 'perl', 'php', 'pike', 'pogoscript', 'powershell', 'processing', 'prolog', 'puppet',
+'python', 'r', 'racket', 'rdoc', 'realbasic', 'rebol', 'robotframework', 'rouge', 'ruby', 'rust',
+'scala', 'scheme', 'scilab', 'self', 'shell', 'slash', 'smalltalk', 'squirrel', 'supercollider',
+'tcl', 'tex', 'turing', 'txl', 'typescript', 'unrealscript', 'vala', 'verilog', 'vhdl', 'viml',
+'volt', 'wisp', 'xbase', 'xc', 'xml', 'xproc', 'xquery', 'xslt', 'xtend']
 
 
 def login(status, args=None):
-    print('\nThis request needs user authentication:')
+    print('\nThis request needs user authentication:\n')
     message = None
     while status == 401:
         if message:
@@ -57,17 +54,14 @@ def login(status, args=None):
             username = input(prompt.encode('utf-8'))
         else:
             username = input(prompt.encode('utf-8')).decode('utf-8')
-
         password = getpass("Github password (hidden): ")
-
         s = requests.Session()
         s.auth = (username, password)
         r = s.get(urljoin(GITHUB_USERS, args['<username>']))
         status = r.status_code
         if status == 404:
             break
-        print("status is: ", status, sep=' ')
-        message = 'Incorrect username or password. Try again:'
+        message = '\nIncorrect username or password. Try again:\n'
 
     return s
 
@@ -79,7 +73,6 @@ def sanitize_qualifiers(repos=None, followers=None, language=None):
     '''
     try:
         repos = repos
-        print(repos, 'nooo')
     except ValueError as e:
         repos = None
     except TypeError as e:
@@ -96,20 +89,25 @@ def sanitize_qualifiers(repos=None, followers=None, language=None):
 
     if repos:
         qualifiers += 'repos:{} '.format(repos)
+        qualifiers = re.sub(r"^([+])([=a-zA-Z0-9]+)$", r">\2", qualifiers)
+        qualifiers = re.sub(r"^([-])([=a-zA-Z0-9]+)$", r"<\2", qualifiers)
 
     if followers:
         qualifiers += 'followers:{} '.format(followers)
+        qualifiers = re.sub(r"^([+])([=a-zA-Z0-9]+)$", r">\2", qualifiers)
+        qualifiers = re.sub(r"^([-])([=a-zA-Z0-9]+)$", r"<\2", qualifiers)
 
-    if language in ALLOWED_LANGUAGES and not language == '':
-        qualifiers += 'language:{} '.format(language)
-    elif language == '':
-        qualifiers += ''
-    else:
-        print(language.title(), "is not a allowed language.")
-        raise AllowedLanguagesException 
+    try:
+        if language in ALLOWED_LANGUAGES and not language == '':
+            qualifiers += 'language:{} '.format(language)
+        elif language == '':
+            qualifiers += ''
+        else:
+            raise AllowedLanguagesException
+    except AllowedLanguagesException as e:
+        print(language.title(), "is not defined by Github.")
 
-    print(qualifiers, 'yes')
-    return qualifiers.replace('+', '>').replace('-', '<')
+    return qualifiers
 
 
 def validate_username(username):
